859971ba6966f5d68c67bdd5532543dffcfc765f
qwerty1954 <qwerty0726@hotmail.com>
2016-11-23 13:44:48 -0800

    Added the start game function and game info stuff

--- Cloud's Functions.py
+++ Cloud's Functions.py
@@ -1,9 +1,56 @@
 def start_game():
-    yes_no = input(print("Do you want play? (y/n): "))
-    while (yes_no.lower() != y or yes_no.lower() != n):
-        yes_no = input(print("What you typed was not y or n. Please try again: "))
-    if yes_no.lower() == n:
-        print ("That's okay. Maybe you'll play next time. Bye!")
-        return
-    
-    
+    yes_no = input("Do you want play? (y/n): ")
+    while (yes_no.lower() != "y" and yes_no.lower() != "n"):
+        yes_no = input("\nWhat you typed was not y or n. Please try again: ")
+    if yes_no.lower() == "n":
+        print ("\nThat's okay. Maybe you'll play next time. Bye!")
+    else:
+        print ("\nGreat! Let's get started then.")
+    return yes_no
+
+
+def game_info():
+    print ("Now we'll fill in the data for you.")
+    name = input("\nWhat is your name? ")
+
+    civ_lvl = input("\nHow civilized are you? (0 - 3): ")
+    while (civ_lvl.isdigit() == False or int(civ_lvl) < 0 or int(civ_lvl) > 3):
+        if civ_lvl.isdigit() == False:
+            civ_lvl = input("That is not a positive integer. Please try again: ")
+        else:
+            civ_lvl = input("That value is not within the valid range. Please try again: ")
+
+
+    fuel_amount = input("\nHow much fuel do you start with? (10 - 50): ")
+    while (fuel_amount.isdigit() == False or int(fuel_amount) < 10 or int(fuel_amount) > 50):
+        if fuel_amount.isdigit() == False:
+            fuel_amount = input("That is not a positive integer. Please try again: ")
+        else:
+            fuel_amount = input("That value is not within the valid range. Please try again: ")
+
+
+    max_turns = input("\nWhat is the maximum amount of turns this game? (1 - 10): ")
+    while (max_turns.isdigit() == False or int(max_turns) < 0 or int(max_turns) > 10):
+        if max_turns.isdigit() == False:
+            max_turns = input("That is not a positive integer. Please try again: ")
+        else:
+            max_turns = input("That value is not within the valid range. Please try again: ")
+
+
+    amaz_expl = input("\nDo you want amazing explosions to happen? (y/n): ")
+    while (amaz_expl.lower() != "y" and amaz_expl.lower() != "n"):
+        amaz_expl = input("What you typed was not y or n. Please try again: ")
+
+    if amaz_expl.lower() == "n":
+        mild_expl = input("\nSince you don't want amazing explosions to happen, do you want mild explosions to happen? (y/n): ")
+        while (mild_expl.lower() != "y" and mild_expl.lower() != "n"):
+            mild_expl = input("What you typed was not y or n. Please try again: ")
+    else:
+        mild_expl = "y"
+
+    return name, civ_lvl, fuel_amount, max_turns, amaz_expl, mild_expl
+
+
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------#
