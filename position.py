3c4d3f6faefb4b255ad1508985fe1bf212b1fa0e
Vincent <vdy@sfu.ca>
2016-11-30 12:49:04 -0800

    Updated roll die

--- position.py
+++ position.py
@@ -1,17 +1,22 @@
 def roll_die(pos,planet_number):
     rand = r.randint(1,6)
-    print(rand)
     res = rand + pos
     while res >= len(planet_number):
         res -= len(planet_number)
+    print("the die was... " + str(rand))
+    print("the previous position was... " + str(pos))
+    print("and the next position is... " + str(res))
     return res
 
 #-------------------------------------------------------
 ##Returns the position of the player after you roll the die.
-
+##Bottom is example
 import random as r
 
-pos = 5
-planet_number = [0,1,2,3,4,5,6,7]
-print(roll_die(pos,planet_number))
+pos = 0
+planet_number = [0,1,2,3]
+pos = roll_die(pos,planet_number)
+pos = roll_die(pos,planet_number)
+pos = roll_die(pos,planet_number)
+pos = roll_die(pos,planet_number)
 
